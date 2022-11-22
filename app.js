29e189c791db4983d89455e23fb238d7747a732c
Sultan <farrelsultan2021@gmail.com>
2022-11-22 17:45:07 +0700

    edit port to local 3000

--- app.js
+++ app.js
@@ -146,13 +146,6 @@ app.get("/about", function(req, res){
 });
 
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port);
-
-
-app.listen(port, function() {
+app.listen(3000, function() {
   console.log("Server started");
 });
